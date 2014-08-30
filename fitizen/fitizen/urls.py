d824dda6149eccb068d0ca618771221d6d97f3cb
John Shiver <johneshiver@gmail.com>
2014-08-29 20:49:24 -0700

    Added css to register user + detail user pages, made some small fixes

--- fitizen/fitizen/urls.py
+++ fitizen/fitizen/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^contact', Contact.as_view(), name='contact'),
+    url(r'^register', CreateUser.as_view(), name='register'),
     url(r'/user/(?P<pk>\d+)/$', DetailUser.as_view(), name="detail_user"),
     url(r'accounts/', include('authtools.urls')),
     url(r'^admin/', include(admin.site.urls)),
