73d2dedf42db4b9c561c408acca345edcefef136
John Shiver <johneshiver@gmail.com>
2014-08-29 17:13:09 -0700

    User can now sign in with name, redirects to confirmation page

--- fitizen/profiles/models.py
+++ fitizen/profiles/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
-from authtools.models import AbstractEmailUser
+from authtools.models import AbstractNamedUser
 
 
-class Fitizen(AbstractEmailUser):
+class Fitizen(AbstractNamedUser):
 
     def get_absolute_url(self):
         return reverse('detail_user', kwargs={"pk": self.pk})
