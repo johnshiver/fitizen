9f4dcd9deff8218e686278814d891e6619680977
John Shiver <johneshiver@gmail.com>
2014-08-30 22:45:23 -0700

    Added more to workout models, about half done

--- fitizen/body_weight_workout/models.py
+++ fitizen/body_weight_workout/models.py
@@ -1,10 +1,40 @@
 from django.db import models
-from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
+
+def validate_max(value):
+    if value > 500:
+        raise ValidationError(u'There is no way you did %s reps' % value)
 
 
 class BodyWeightWorkout(models.Model):
     """
     Represents a single workout, assumes one workout per day
     """
-    user = owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(User, unique_for_date='date_created')
     date_created = models.DateTimeField(auto_now_add=True)
+
+
+class WeightExercise(models.Model):
+    """
+    Represents a single Exercise
+    """
+    PULLUP = 'PU'
+    DIP = 'D'
+    SQUAT = 'SQ'
+    LSIT = 'LS'
+    PUSHUP = 'PU'
+    ROW = "RW"
+
+    EXERCISE_CHOICES = (
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+    )
+
+    workout = models.ForeignKey(BodyWeightWorkout)
+    reps = models.PositiveSmallIntegerField()
+    exercise =
+    which_set =
