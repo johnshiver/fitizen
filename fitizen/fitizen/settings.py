aa533930618e66bae14ff0a36a3d4b17654e0f97
John Shiver <johneshiver@gmail.com>
2014-09-03 16:44:31 -0700

    Changed db settings just for testing purpose

--- fitizen/fitizen/settings.py
+++ fitizen/fitizen/settings.py
@@ -74,18 +74,29 @@ class Base(Configuration):
     # Database
     # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #         'NAME': 'fitizen',
+    #         'USER': 'johnshiver',
+    #         'PASSWORD': '',
+    #         'HOST': 'localhost',
+    #         'PORT': '5432',
+    #     }
+    # }
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+    TEMPLATE_DEBUG = False
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'fitizen',
-            'USER': 'johnshiver',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'NAME': os.environ.get('DJANGO_DB_NAME'),
+            'USER': os.environ.get('DJANGO_DB_USER'),
+            'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD'),
+            'HOST': os.environ.get('DJANGO_DB_HOST'),
         }
     }
-    # Internationalization
-    # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
     LANGUAGE_CODE = 'en-us'
 
