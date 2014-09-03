ae983e35d8dd8ea5c5ea2b8140ee76976126cfb1
John Shiver <johneshiver@gmail.com>
2014-09-03 16:05:00 -0700

    Updated settings and reqs for deployment

--- fitizen/fitizen/settings.py
+++ fitizen/fitizen/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from configurations import Configuration
 from unipath import Path
 
 # Normally should not import ANYTHING from Django directly
@@ -17,97 +18,115 @@ from unipath import Path
 from django.core.exceptions import ImproperlyConfigured
 
 
-def get_env_variable(var_name):
-    """Get the env variable or return exception"""
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        error_msg = "Set the %s environment variable" % var_name
-        raise ImproperlyConfigured(error_msg)
+class Base(Configuration):
 
+    def get_env_variable(var_name):
+        """Get the env variable or return exception"""
+        try:
+            return os.environ[var_name]
+        except KeyError:
+            error_msg = "Set the %s environment variable" % var_name
+            raise ImproperlyConfigured(error_msg)
 
-BASE_DIR = Path(__file__).ancestor(3)
+    BASE_DIR = Path(__file__).ancestor(3)
 
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = '2atktt@f*o!-_ul$k(-t26gt7%i6i)!!28-md27pv^ehob2ys8'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2atktt@f*o!-_ul$k(-t26gt7%i6i)!!28-md27pv^ehob2ys8'
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    TEMPLATE_DEBUG = True
 
-TEMPLATE_DEBUG = True
+    ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
+    # Application definition
 
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'south',
+        'body_weight_workout',
+        'profiles',
+        'crispy_forms',
+    )
 
-# Application definition
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'south',
-    'body_weight_workout',
-    'profiles',
-    'crispy_forms',
-)
+    ROOT_URLCONF = 'fitizen.urls'
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+    WSGI_APPLICATION = 'fitizen.wsgi.application'
 
-ROOT_URLCONF = 'fitizen.urls'
+    # Database
+    # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-WSGI_APPLICATION = 'fitizen.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fitizen',
-        'USER': 'johnshiver',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'fitizen',
+            'USER': 'johnshiver',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+    TIME_ZONE = 'UTC'
 
-USE_I18N = True
+    USE_I18N = True
 
-USE_L10N = True
+    USE_L10N = True
 
-USE_TZ = True
+    USE_TZ = True
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+    MEDIA_ROOT = BASE_DIR.child("media")
+
+    STATICFILES_DIRS = (
+        BASE_DIR.child("static"),
+    )
+
+    STATIC_URL = '/static/'
+
+    CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+class Dev(Base):
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    TEMPLATE_DEBUG = True
 
-MEDIA_ROOT = BASE_DIR.child("media")
 
-STATICFILES_DIRS = (
-    BASE_DIR.child("static"),
-)
-
-STATIC_URL = '/static/'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+class Prod(Base):
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    TEMPLATE_DEBUG = False
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('DJANGO_DB_NAME'),
+            'USER': os.environ.get('DJANGO_DB_USER'),
+            'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD'),
+            'HOST': os.environ.get('DJANGO_DB_HOST'),
+        }
+    }
