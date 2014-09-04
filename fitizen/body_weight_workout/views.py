aca07dac243597f8da9940d39331508723dc1fbf
John Shiver <johneshiver@gmail.com>
2014-09-04 13:45:03 -0700

    Can now choose which workout you do, changed date

--- fitizen/body_weight_workout/views.py
+++ fitizen/body_weight_workout/views.py
@@ -85,6 +85,36 @@ class UpdateWorkout(
     View
 ):
 
+    def set_choices(self, group_number):
+
+        EXERCISE_CHOICES = {
+                1: [
+                    (('1', 'Level 1: Wall Pushup'),
+                     ('2', 'Level 2: Incline Pushup'),
+                     ('3', 'Level 3: Pushup'),
+                     ('4', 'Level 4: Diamond Pushup')),
+                    (('1', 'Level 1: Vertical Row'),
+                     ('2', 'Level 2: Incline Row'),
+                     ('3', 'Level 3: Row'))],
+                2: [
+                    (('1', 'Level 1: Wall Pushup'),
+                     ('2', 'Level 2: Incline Pushup'),
+                     ('3', 'Level 3: Pushup'),
+                     ('4', 'Level 4: Diamond Pushup')),
+                    (('1', 'Level 1: Vertical Row'),
+                     ('2', 'Level 2: Incline Row'),
+                     ('3', 'Level 3: Row'))],
+                3: [
+                    (('1', 'Level 1: Wall Pushup'),
+                     ('2', 'Level 2: Incline Pushup'),
+                     ('3', 'Level 3: Pushup'),
+                     ('4', 'Level 4: Diamond Pushup')),
+                    (('1', 'Level 1: Vertical Row'),
+                     ('2', 'Level 2: Incline Row'),
+                     ('3', 'Level 3: Row'))]
+        }
+        return EXERCISE_CHOICES[group_number]
+
     def get(self, request, *args, **kwargs):
         username = self.kwargs['username']
         if request.user.username == username:
@@ -92,9 +122,12 @@ class UpdateWorkout(
                          3: 'update_group3.html'}
             workout_id = self.kwargs['workout_id']
             group = self.kwargs['group']
+            choices = self.set_choices(int(group))
             template = templates[int(group)]
-            form1 = WeightExerciseForm(prefix='ex1')
-            form2 = WeightExerciseForm(prefix='ex2')
+            form1 = WeightExerciseForm(view_choices=choices[0],
+                                       prefix='ex1')
+            form2 = WeightExerciseForm(view_choices=choices[1],
+                                       prefix='ex2')
             return render(request, template, {
                           'form1': form1,
                           'form2': form2,
@@ -105,19 +138,24 @@ class UpdateWorkout(
             return redirect('home')
 
     def post(self, request, *args, **kwargs):
-        form1 = WeightExerciseForm(request.POST, prefix='ex1')
-        form2 = WeightExerciseForm(request.POST, prefix='ex2')
+        group = self.kwargs['group']
+        choices = self.set_choices(int(group))
+        form1 = WeightExerciseForm(view_choices=choices[0], data=request.POST, prefix='ex1')
+        form2 = WeightExerciseForm(view_choices=choices[1], data=request.POST, prefix='ex2')
         groups = {1: ('PL', 'D'), 2: ('SQ', 'LS'), 3: ('PU', 'RW')}
         if form1.is_valid() and form2.is_valid():
             workout_id = int(self.kwargs['workout_id'])
             group_number = int(self.kwargs['group'])
             w1 = WeightExercise.objects.get(workout=workout_id, exercise=groups[group_number][0])
             w2 = WeightExercise.objects.get(workout=workout_id, exercise=groups[group_number][1])
-            f1 = WeightExerciseForm(request.POST, instance=w1, prefix='ex1')
-            f2 = WeightExerciseForm(request.POST, instance=w2, prefix='ex2')
+            f1 = WeightExerciseForm(view_choices=choices[0], data=request.POST, instance=w1, prefix='ex1')
+            f2 = WeightExerciseForm(view_choices=choices[1], data=request.POST, instance=w2, prefix='ex2')
             f1.save()
             f2.save()
             return HttpResponseRedirect('/' + request.user.username + '/workout/' + str(workout_id))
         else:
-            form1 = WeightExerciseForm()
-            form2 = WeightExerciseForm()
+            workout_id = self.kwargs['workout_id']
+            group = self.kwargs['group']
+            print request.POST
+            self.messages.success("Invalid!")
+            return HttpResponseRedirect('/' + request.user.username + '/update_workout/' + str(workout_id) + '/' + str(group))
